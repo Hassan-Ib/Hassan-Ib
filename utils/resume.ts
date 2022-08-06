75d9d0bfe628b1fdc05cf6b19311603dc03a128c
Hassan-Ib <azanebrahim@gmail.com>
2022-08-06 10:28:10 -0600

    BS 1

--- utils/resume.ts
+++ utils/resume.ts
@@ -1,14 +1,35 @@
 import { saveAs } from "file-saver";
 export const downloadCv = async () => {
-  // const url = "/api/download_resume";
-  const url = "/api/hello";
+  const url = "/api/download_resume";
+  //   const url = "/api/hello";
 
   try {
     const response = await fetch(url);
     if (!response.ok) throw response;
+
     console.log(response.status);
     console.log("hello", response.body);
-    console.log("json", await response.json());
+
+    const blob = await response.blob();
+    console.log("json", blob);
+
+    let fileReader = new FileReader();
+
+    fileReader.readAsDataURL(blob);
+
+    fileReader.addEventListener("load", (event) => {
+      var uri = event?.target?.result;
+      if (!uri || typeof uri !== "string") {
+        throw new Error("No uri");
+      }
+      var link = document.createElement("a");
+      link.download = "Ibrahim Hassan Resumed.pdf";
+      link.href = uri;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      //   delete link;
+    });
     // const blob = await response.blob();
     // console.log(blob);
     // saveAs(blob, "HassanIbrahimResume.pdf");
