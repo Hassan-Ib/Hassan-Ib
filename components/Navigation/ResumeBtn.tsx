ceece6b8023e0c71591db67c7ac91ef5f90f42b7
Hassan-Ib <azanebrahim@gmail.com>
2022-08-06 00:00:57 -0600

    fixed resume download

--- components/Navigation/ResumeBtn.tsx
+++ components/Navigation/ResumeBtn.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { EventHandler, MouseEvent, KeyboardEvent } from "react";
+import { saveAs } from "file-saver";
 
 export function ResumeBtn({}) {
+  const save = async (
+    e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>
+  ) => {
+    const url = "/api/download-cv";
+    try {
+      const response = await fetch(url);
+      console.log(response.status);
+      console.log(response.body);
+      const blob = await response.blob();
+      saveAs(blob, "HassanIbrahimResume.pdf");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <button className=" border  border-black dark:border-main-100 font-medium font-barlow tracking-wider px-6 py-1 rounded-md capitalize hover:bg-main-100/20">
+    <button
+      onClick={save}
+      className=" border  border-black dark:border-main-100 font-medium font-barlow tracking-wider px-6 py-1 rounded-md capitalize hover:bg-main-100/20">
       resume
     </button>
   );
