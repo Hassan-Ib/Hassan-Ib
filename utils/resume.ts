067a14dcaf01de48b65ed0cebcad98365bf210a7
Hassan-Ib <azanebrahim@gmail.com>
2022-08-06 09:11:01 -0600

    BS

--- utils/resume.ts
+++ utils/resume.ts
@@ -1,15 +1,18 @@
 import { saveAs } from "file-saver";
 export const downloadCv = async () => {
-  const url = "/api/download_resume";
+  // const url = "/api/download_resume";
+  const url = "/api/hello";
+
   try {
     const response = await fetch(url);
     if (!response.ok) throw response;
     console.log(response.status);
-    console.log(response.body);
-    const blob = await response.blob();
-    console.log(blob);
-    saveAs(blob, "HassanIbrahimResume.pdf");
+    console.log("hello", response.body);
+    console.log("json", await response.json());
+    // const blob = await response.blob();
+    // console.log(blob);
+    // saveAs(blob, "HassanIbrahimResume.pdf");
   } catch (error) {
-    console.log(error);
+    console.log("error", error);
   }
 };
