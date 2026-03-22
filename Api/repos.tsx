37b0f7b2f2c9c31f64d1125b922b09acd3397364
ibrahim hassan <ibrahim.ayomide@mariopsys.com>
2026-03-22 18:21:23 +0100

    Add logging for GITHUB_TOKEN and GITHUB_OWNER in repos.tsx

--- Api/repos.tsx
+++ Api/repos.tsx
@@ -1,5 +1,6 @@
 import { Octokit } from "@octokit/rest";
-
+console.log("GITHUB_TOKEN:", process.env.GITHUB_TOKEN);
+console.log("GITHUB_OWNER:", process.env.GITHUB_OWNER);
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
@@ -10,8 +11,8 @@ export const getRepos = (repoList: string[]) => {
       octokit.rest.repos.get({
         owner: process.env.GITHUB_OWNER!,
         repo,
-      })
-    )
+      }),
+    ),
   )
     .then((reposData) => reposData)
     .catch((err) => {
